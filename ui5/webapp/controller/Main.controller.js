92a0ed984129f1f004ee7ead024de3fa0e8becca
Benjamin Krencker <benjamin.krencker@emmi.ch>
2019-11-15 13:36:16 +0000

    everything is working without errors

---
+++ ui5/webapp/controller/Main.controller.js
@@ -0,0 +1,16 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("ui5.error.test.ui5.controller.Main", {
+		onInit: function () {
+
+		},
+		
+		onUserSelection: function(oEvent) {
+			var oContext = oEvent.getParameter("rowContext");
+			this.byId("idFriendsTable").setBindingContext(oContext);
+		}
+	});
+});
